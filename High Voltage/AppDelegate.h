c714b74d28b18061526e0ec4ea63aab22bce5b48
Shane Nelson <shanenelson@shanes-macbook-pro.local>
2017-04-26 16:44:39 -0400

    Initial Commit

---
+++ High Voltage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  High Voltage
+//
+//  Created by Shane Nelson on 4/26/17.
+//  Copyright © 2017 Shane Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ High Voltage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  High Voltage
+//
+//  Created by Shane Nelson on 4/26/17.
+//  Copyright © 2017 Shane Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
